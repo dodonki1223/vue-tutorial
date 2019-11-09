952e14a23f6452f9c89d79b44ca7a2bf45eb8228
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-11-09 18:03:39 +0900

    :sparkles: カスタムイベント - イベント名、v-modelを使ったコンポーネントのカスタマイズ

---
+++ 15_custom_event/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MyComponent
+      v-on:welcome="sayHi" 
+      v-on:my-event="sayHi"
+      v-model="lovingVue"
+      v-on:change="nowChecked"
+    ></MyComponent>
+  </div>
+</template>
+
+<script>
+import MyComponent from './components/MyComponent.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MyComponent
+  },
+  data: function() {
+    return {
+      lovingVue: {
+        checked: true
+      }
+    }
+  },
+  methods: {
+    sayHi: function() {
+      alert('Hi')
+    },
+    nowChecked: function(checked) {
+      alert(checked)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 15_custom_event/src/components/MyComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h2>カスタムイベント</h2>
+    <!-- イベント名 -->
+    <div>
+      <div>
+        <p>動くイベント</p>
+        <button v-on:click="$emit('welcome')">
+          Click me to be webcomed
+        </button>
+      </div>
+      <div>
+        <!-- 
+          v-onイベントリスナは自動的に小文字に変換されます（HTMLが大文字小文字を判別しないため） 
+          v-on:myEventはv-on:myeventになりmyEventにリスナが反応することができなくなります
+          上記理由から `いつもケバブケース(kebab-case)`を使うことをお薦めします
+        -->
+        <p>動かないイベント</p>
+        <button v-on:click="$emit('myEvent')">
+          Click me to be webcomed
+        </button>
+      </div>
+    </div>
+    <!-- v-model を使ったコンポーネントのカスタマイズ -->
+    <div>
+      <input
+        type="checkbox"
+        v-bind:checked="checked"
+        v-on:change="$emit('change', $event.target.checked)"
+      >
+      <p>現在のチェックボックスの値：{{ checked }}</p>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'checked',
+    event: 'change'
+  },
+  props: {
+    checked: Boolean
+  }
+}
+</script>
