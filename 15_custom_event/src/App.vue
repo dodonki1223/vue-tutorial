324e04c52568945354e0a4710388b4b614525612
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-11-12 09:49:18 +0900

    :sparkles: カスタムイベント - .sync修飾子

--- 15_custom_event/src/App.vue
+++ 15_custom_event/src/App.vue
@@ -1,32 +1,49 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <!-- v-modelを使ったコンポーネントのカスタマイズ -->
     <MyComponent
       v-on:welcome="sayHi" 
       v-on:my-event="sayHi"
       v-model="lovingVue"
       v-on:change="nowChecked"
     ></MyComponent>
+    <!-- コンポーネントにネイティブイベントをバインディング -->
     <BaseInput
       v-model="label"
     ></BaseInput>
+    <!-- .sync修飾子 -->
+    <!-- 子供のコンポーネントから親のプロパティ値を更新することができる -->
+    <div>
+      <!-- 単体の値の場合 -->
+      <TextDocument v-bind:title.sync="doc.title"></TextDocument>
+      <!-- オブジェクトの値をすべて与える場合 -->
+      <TextDocument v-bind.sync="doc"></TextDocument>
+      <span>大人だよ：<input v-model="doc.title"></span>
+    </div>
   </div>
 </template>
 
 <script>
 import MyComponent from './components/MyComponent.vue'
 import BaseInput from './components/BaseInput.vue'
+import TextDocument from './components/TextDocument.vue'
 
 export default {
   name: 'app',
   components: {
     MyComponent,
-    BaseInput
+    BaseInput,
+    TextDocument
   },
   data: function() {
     return {
       lovingVue: true,
       label: 'ゴリラ',
+      doc: {
+        title: 'キリン',
+        animal: 'カバ'
+      }
     }
   },
   methods: {
@@ -36,6 +53,9 @@ export default {
     nowChecked: function(checked) {
       alert(checked)
     },
+    handleChange: function(event) {
+      this.doc.title = event.target.value
+    }
   }
 }
 </script>
---
+++ 15_custom_event/src/components/TextDocument.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span>子供だよ：<button @click="updateValue">Reverse String Update {{ title }}</button></span>
+    <p>{{ animal }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    animal: String
+  },
+  methods: {
+    updateValue: function() {
+      this.$emit('update:title', this.title.split('').reverse().join(''))
+    }
+  }
+}
+</script>
