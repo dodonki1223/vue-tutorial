a2834aebf96ebbcbc300aabc737058586e71a96a
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-11-13 09:29:13 +0900

    :sparkles: スロット - スロットコンテンツ

--- 16_slot/src/App.vue
+++ 16_slot/src/App.vue
@@ -2,20 +2,33 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>スロット</h1>
+    <div>
+      
+      <navigation-link url="/profile">
+        <!-- Font Awesome のアイコンを追加 -->
+        <span class="fa fa-user"></span>
+        <!-- コンポーネントを使ってアイコンを追加 -->
+        <font-awesome-icon icon="user" />
+        Your Profile
+      </navigation-link>
+    </div>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import NavigationLink from './components/NavigationLink.vue'
 
 export default {
   name: 'app',
   components: {
+    NavigationLink
   }
 }
 </script>
 
 <style>
+/* CDNでFont Awesomeを読み込む方法 */
+@import "https://use.fontawesome.com/releases/v5.1.0/css/all.css";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- 16_slot/src/main.js
+++ 16_slot/src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Font Awesomeの設定を追加
+// この前に以下のコマンドを叩くこと
+// npm install --save @fortawesome/fontawesome-svg-core
+// npm install --save @fortawesome/free-solid-svg-icons
+// npm install --save @fortawesome/vue-fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+// userのアイコンを追加（１つずつしか読み込めないかも……）
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUser)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
