e9cba0f2f9bb640c466dfd58cc52eb19be3560bd
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-11-13 10:03:43 +0900

    :sparkles: スロット - 名前付きスロット

--- 16_slot/src/App.vue
+++ 16_slot/src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>スロット</h1>
+    <!-- スロットコンテンツ -->
     <div>
-      <!-- スロットコンテンツ -->
       <navigation-link url="/profile">
         <!-- Font Awesome のアイコンを追加 -->
         <span class="fa fa-user"></span>
@@ -12,8 +12,8 @@
         Your Profile
       </navigation-link>
     </div>
+    <!-- コンパイルスコープ -->
     <div>
-      <!-- コンパイルスコープ -->
       <!-- 
         親テンプレート内のすべてのものは親のスコープでコンパイルされ、
         子テンプレート内の全てのものは子のスコープでコンパイルされる 
@@ -34,12 +34,47 @@
         </navigation-link>
       </div>
     </div>
+    <!-- フォールバックコンテンツ -->
     <div>
-      <!-- フォールバックコンテンツ -->
       <!-- コンテンツの指定なし -->
       <submit-button></submit-button>
       <!-- コンテンツの指定 -->
       <submit-button>Save</submit-button>
+    </div>
+    <!-- 名前付きスロット -->
+    <div>
+      <!-- デフォルトのスロットに何も指定しないバージョン -->
+      <div>
+        <base-layout>
+          <template v-slot:header>
+            <h1>Here might be a page title</h1>
+          </template>
+
+          <p>A paragraph for the main content.</p>
+          <p>And another one.</p>
+
+          <template v-slot:footer>
+            <p>Here's some contact info</p>
+          </template>
+        </base-layout>
+      </div>  
+      <!-- デフォルトのスロットに指定するバージョン -->
+      <div>
+        <base-layout>
+          <template v-slot:header>
+            <h1>Here might be a page title</h1>
+          </template>
+
+          <template v-slot:default>
+            <p>A paragraph for the main content.</p>
+            <p>And another one.</p>            
+          </template>
+
+          <template v-slot:footer>
+            <p>Here's some contact info</p>
+          </template>
+        </base-layout>
+      </div>
     </div>
   </div>
 </template>
@@ -47,12 +82,14 @@
 <script>
 import NavigationLink from './components/NavigationLink.vue'
 import SubmitButton from './components/Button.vue'
+import BaseLayout from './components/BaseLayout.vue'
 
 export default {
   name: 'app',
   components: {
     NavigationLink,
-    SubmitButton
+    SubmitButton,
+    BaseLayout
   },
   data: function() {
     return {
---
+++ 16_slot/src/components/BaseLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <!-- nameのないslotは暗黙的にdefaultという名前を持つ -->
+      <slot></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
