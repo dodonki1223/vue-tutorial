6b3aa556af40ae8160ea9f6fef7884742356bf18
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-05 08:03:59 +0900

    :memo: 21.Passing your own Arguments with Events

--- vue-js-2-the-complete-guide/section2/2-2/app.js
+++ vue-js-2-the-complete-guide/section2/2-2/app.js
@@ -6,10 +6,11 @@ new Vue({
     y: 0,
   },
   methods: {
-    increase: function() {
-      this.counter++
+    increase: function(step, event) {
+      this.counter += step;
     },
     // v-on ディレクティブの場合、引数としてeventオブジェクトにアクセスすることができる
+    // eventはVue.jsで自動的に作成される引数になる
     updateCoordinates: function(event) {
       this.x = event.clientX
       this.y = event.clientY
