4a2f36bb36608c91b4935b03cfc5ee257c5c5ddc
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-05 07:54:52 +0900

    :memo: 20. Getting Event Data from the Event Object

--- vue-js-2-the-complete-guide/section2/2-2/app.js
+++ vue-js-2-the-complete-guide/section2/2-2/app.js
@@ -1,11 +1,18 @@
 new Vue({
   el: '#app',
   data: {
-    counter: 0
+    counter: 0,
+    x: 0,
+    y: 0,
   },
   methods: {
     increase: function() {
       this.counter++
-    }
+    },
+    // v-on ディレクティブの場合、引数としてeventオブジェクトにアクセスすることができる
+    updateCoordinates: function(event) {
+      this.x = event.clientX
+      this.y = event.clientY
+    },
   }
 })
