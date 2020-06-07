6d199c6206fc9564b52163fe260003d6b076c945
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-07 16:48:39 +0900

    :memo: 26.Reacting to Changes with Computed Properties

--- vue-js-2-the-complete-guide/section2/2-2/app.js
+++ vue-js-2-the-complete-guide/section2/2-2/app.js
@@ -2,10 +2,23 @@ new Vue({
   el: '#app',
   data: {
     counter: 0,
+    secondCounter: 0,
     x: 0,
     y: 0,
   },
+  // プロパティのように扱います
+  computed: {
+    output: function() {
+      console.log('Computed')
+      return this.counter > 5 ? 'Greater 5' : 'Smaller than 5'
+    }
+  },
   methods: {
+    // ES6の書き方なのでこちらを採用すると良さそう！！
+    result() {
+      console.log('Methods')
+      return this.counter > 5 ? 'Greater 5' : 'Smaller than 5'
+    },
     increase: function(step, event) {
       this.counter += step;
     },
