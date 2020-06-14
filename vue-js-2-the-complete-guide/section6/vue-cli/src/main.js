f67c81460f1c6ad552b0ead6aefa85075b5f9a55
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-14 22:03:59 +0900

    :sparkles: 88.Understanding ".vue" Files

--- vue-js-2-the-complete-guide/section6/vue-cli/src/main.js
+++ vue-js-2-the-complete-guide/section6/vue-cli/src/main.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+/*
+  以下のQiitaの記事にまさにの説明がある
+    https://qiita.com/teinen_qiita/items/ed1bb1909a17f9ca9daa
+  この記述の意味がよくわからない……
+    new Vue({
+      el: '#app',
+      render: h => h(App)
+    })
+  こういうことらしい
+    new Vue({
+      el: '#app',
+      components: { App },
+      template: '<App />'
+    })
+ */
 new Vue({
   el: '#app',
   render: h => h(App)
