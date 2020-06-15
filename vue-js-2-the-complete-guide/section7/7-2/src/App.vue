c55096ad893af212c049b530bf269dbed05e26cd
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-16 08:32:33 +0900

    :memo: 99.Creating a Component

---
+++ vue-js-2-the-complete-guide/section7/7-2/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <app-server-status></app-server-status>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-js-2-the-complete-guide/section7/7-2/src/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- 
+    template 直下は必ずたった１つの要素だけにすること 
+    そうしないとトランスパイル出来なくて落ちる
+    -->
+  <div>
+    <p>Server Status: {{ status }}</p>
+    <hr>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      status: 'Critical'
+    }
+  },
+  methods: {
+    /*
+      changeStatus: function()の書き方でなくて下記のような書き方でもOK
+      ES6からはObjectに関数を記述することができるようになったため、以下の書き方でOK
+     */
+    changeStatus() {
+      this.status = 'Normal'
+    }
+  }
+}
+</script>
