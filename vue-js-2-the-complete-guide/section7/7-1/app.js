58739a6735c563cd5847c61cb817080683f8ad80
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-15 23:36:05 +0900

    :memo: 96.Storing Data in Components with the Data Method

--- vue-js-2-the-complete-guide/section7/7-1/app.js
+++ vue-js-2-the-complete-guide/section7/7-1/app.js
@@ -1,3 +1,5 @@
+var data = { status: 'Critical' }
+
 Vue.component('my-cmp', {
   /*
       new Vueの時は下記のような記述だったのにComponentでは関数なのか？
@@ -10,11 +12,25 @@ Vue.component('my-cmp', {
       データに干渉しないようにするためである
    */
   data: function() {
+    /*
+        var data = { status: 'Critical' } を使用すると
+        Vue.component('my-cmp', {
+          data: return data
+        })
+        上記のようにすると……
+        ２つあるコンポーネントのボタンをクリックすると両方とも更新されてしまう
+        なので　data は必ず関数で返す必要がある
+     */
     return {
       status: 'Critical'
     }
   },
-  template: '<p>Server Status: {{ status }}</p>'
+  template: '<p>Server Status: {{ status }} (<button @click="changeStatus">Change</button>)</p>',
+  methods: {
+    changeStatus: function() {
+      this.status = 'Normal'
+    }
+  }
 })
 
 new Vue({
