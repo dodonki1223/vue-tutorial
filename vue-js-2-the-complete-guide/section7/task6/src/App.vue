6c67e16955ab95efc43f2288918fe057dbbd8d8f
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-17 08:28:10 +0900

    :recycle: 課題6をコンポーネントを使用して分割する

--- vue-js-2-the-complete-guide/section7/task6/src/App.vue
+++ vue-js-2-the-complete-guide/section7/task6/src/App.vue
@@ -1,39 +1,42 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-header></app-header>
+      </div>
     </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+          <server-detail v-for="index in 5" :key="index" :serverIndex="index"></server-detail>
+        </ul>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <p>Server Details are currently not updated</p>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12">
+        <app-footer></app-footer>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import ServerDetail from './ServerDetail.vue'
+import AppHeader from './Header.vue'
+import AppFooter from './Footer.vue'
+
+export default {
+  components: {
+    'server-detail': ServerDetail,
+    'app-header': AppHeader,
+    'app-footer': AppFooter,
+  }
+}
 </script>
 
 <style>
---
+++ vue-js-2-the-complete-guide/section7/task6/src/ServerDetail.vue
@@ -0,0 +1,20 @@
+<template>
+  <li class="list-group-item">
+    Server #{{ serverIndex }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    serverIndex: {
+      type: Number,
+      required: Number,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
