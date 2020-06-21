f74e754ec59dd004aa327996443376f40e4df40f
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-21 23:16:01 +0900

    :sparkles: 132.Switching Multiple Components with Dynamic Components

--- vue-js-2-the-complete-guide/section9/9-1/src/App.vue
+++ vue-js-2-the-complete-guide/section9/9-1/src/App.vue
@@ -2,18 +2,24 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <!--  
-          Dataオブジェクトは子コンポーネントで正しくレンダリングされる
-          ただし、style に関しては子コンポーネント側のものしか正しく動作しない
-         -->
-        <app-quote>
-          <!-- 
-            <名前付きslot>
-              slot="title" とすることで 子コンポーネント内の <slot name="title"></slot> の部分にレンダリングされるようになります 
-            -->
+        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <button @click="selectedComponent = 'appAuthor'">Author</button>
+        <button @click="selectedComponent = 'appNew'">New</button>
+        <hr>
+        <p>{{ selectedComponent }}</p>
+        <!-- 
+            <動的コンポーネント>
+              テンプレート内のパーツを別のパーツに置き換える方法
+              component は Vue.js の予約語
+              コンポーネント名で動的に切り替えることができる
+          -->
+        <component :is="selectedComponent">
+          <p>Default Content</p>
+        </component>
+        <!-- <app-quote>
           <h2 slot="title">{{ quoteTitle }}</h2>
           <p>A wonderful Quote</p>
-        </app-quote>
+        </app-quote> -->
       </div>
     </div>
   </div>
@@ -21,15 +27,20 @@
 
 <script>
 import Quote from './components/Quote.vue'
+import Author from './components/Author.vue'
+import New from './components/New.vue'
 
 export default {
   data: function() {
     return {
-      quoteTitle: 'The Quote'
+      quoteTitle: 'The Quote',
+      selectedComponent: 'appQuote',
     }
   },
   components: {
-    appQuote: Quote
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: New,
   }
 }
 </script>
---
+++ vue-js-2-the-complete-guide/section9/9-1/src/components/New.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>The New</h3>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
