8b29f342d4efb539d8314a8fb954969628f14dfc
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-21 23:36:16 +0900

    :sparkles: 134.Keeping Dynamic Components Alive

--- vue-js-2-the-complete-guide/section9/9-1/src/App.vue
+++ vue-js-2-the-complete-guide/section9/9-1/src/App.vue
@@ -12,10 +12,14 @@
               テンプレート内のパーツを別のパーツに置き換える方法
               component は Vue.js の予約語
               コンポーネント名で動的に切り替えることができる
+            keep-aliveを使用することで component 内の切り替えを行っても状態が保持された状態になる
+            Increaseボタンの counter の値が保持された状態になる
           -->
-        <component :is="selectedComponent">
-          <p>Default Content</p>
-        </component>
+        <keep-alive>
+          <component :is="selectedComponent">
+            <p>Default Content</p>
+          </component>
+        </keep-alive>
         <!-- <app-quote>
           <h2 slot="title">{{ quoteTitle }}</h2>
           <p>A wonderful Quote</p>
