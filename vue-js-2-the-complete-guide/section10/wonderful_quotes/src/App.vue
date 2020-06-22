b37b289c7d523cc0862d8246dfa31fe82a77457f
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-22 09:38:54 +0900

    :sparkles: quotes を slot で表示させる

--- vue-js-2-the-complete-guide/section10/wonderful_quotes/src/App.vue
+++ vue-js-2-the-complete-guide/section10/wonderful_quotes/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="container">
-    
+    <quote-grid :quotes="quotes"></quote-grid>
   </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue'
+
 export default {
+  components: {
+    QuoteGrid: QuoteGrid
+  },
   data: function() {
     return {
       quotes: [
-        'Just a Quote to see something'
+        'Just a Quote to see something',
+        'Second Quote'
       ],
       maxQuotes: 10
     }
--- vue-js-2-the-complete-guide/section10/wonderful_quotes/src/components/QuoteGrid.vue
+++ vue-js-2-the-complete-guide/section10/wonderful_quotes/src/components/QuoteGrid.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="row">
+    <quote v-for="(quote, index) in quotes" :key="index">{{ quote }}</quote>
   </div>  
 </template>
 
 <script>
+import Quote from './Quote.vue'
+
 export default {
+  components: {
+    Quote: Quote
+  },
   props: ['quotes']
 }
 </script>
