dcfa07649848efc45d7039d0a745010c76d90fb7
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-22 09:11:05 +0900

    :sparkles: 141. Initializing the Application

--- vue-js-2-the-complete-guide/section10/wonderful_quotes/src/App.vue
+++ vue-js-2-the-complete-guide/section10/wonderful_quotes/src/App.vue
@@ -1,13 +1,20 @@
 <template>
-    <div class="container">
+  <div class="container">
     
-    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+export default {
+  data: function() {
+    return {
+      quotes: [
+        'Just a Quote to see something'
+      ],
+      maxQuotes: 10
     }
+  }
+}
 </script>
 
 <style>
