497b00b1d7911c429a852c9677d1c280ff9df506
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-26 08:48:13 +0900

    :sparkles: 157.Using Radio Buttons

--- vue-js-2-the-complete-guide/section11/11-1/src/App.vue
+++ vue-js-2-the-complete-guide/section11/11-1/src/App.vue
@@ -79,18 +79,25 @@
         </div>
       </div>
       <div class="row">
+        <!--  
+            Vue.jsは両方のラジオボタンが同じグループに属していることを認識する
+            ラジオボタンが同じv-modelでバインドされていることを認識しているため
+            片方のみにしか選択できないようになっている（何もしていないと両方とも選択することができる）
+          -->
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
           <label for="male">
             <input
               type="radio"
               id="male"
-              value="Male"> Male
+              value="Male"
+              v-model="gender"> Male
           </label>
           <label for="female">
             <input
               type="radio"
               id="female"
-              value="Female"> Female
+              value="Female"
+              v-model="gender"> Female
           </label>
         </div>
       </div>
@@ -133,7 +140,7 @@
             <ul>
               <li v-for="item in sendMail" :key="item">{{ item }}</li>
             </ul>
-            <p>Gender:</p>
+            <p>Gender: {{ gender }}</p>
             <p>Priority:</p>
             <p>Switched:</p>
           </div>
@@ -153,7 +160,8 @@ export default {
         age: 27,
       },
       message: "A new text",
-      sendMail: []
+      sendMail: [],
+      gender: 'Male'
     }
   }
 }
