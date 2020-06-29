8356a1b8680b5ec37b203346c42b5481bb4961d6
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-06-29 23:58:48 +0900

    :sparkles: 166.Understanding Directives

---
+++ vue-js-2-the-complete-guide/section12/12-1/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Built-in Directives</h1>
+        <p v-text="'Some Text'"></p>
+        <!--  
+            クロスサイトスクリプティングにあうため、ちゃんとサニタイズされたものが表示されるようにしないと危ないです
+          -->
+        <p v-html="'<strong>Some strong text</strong>'"></p>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-text="'Some Text'"></p>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
