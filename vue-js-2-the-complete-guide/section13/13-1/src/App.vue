54d513eae3dfc2b6180b4a2e4b46225ff7c2d920
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-01 09:37:32 +0900

    :sparkles: 180.Global Filters and How to Chain Multiple Filters

--- vue-js-2-the-complete-guide/section13/13-1/src/App.vue
+++ vue-js-2-the-complete-guide/section13/13-1/src/App.vue
@@ -3,7 +3,13 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
-        <p>{{ text | toUppercase(text) }}</p>
+        <!--  
+            グローバル、ローカルの filters の使用方法
+            toUppercase（ローカル）ですべて大文字に変換し to-lowercase（グローバル）で
+            すべて小文字に変換します
+            下記のやり方が filters のチェーンの仕方でもあります
+         -->
+        <p>{{ text | toUppercase | to-lowercase }}</p>
       </div>
     </div>
   </div>
