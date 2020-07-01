72eacff017af13562853abcc7d1ca539abf8ca94
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-02 08:09:02 +0900

    :sparkles: 192.Preparing Code to use Transitions

---
+++ vue-js-2-the-complete-guide/section14/14-1/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr>
+        <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+        <br>
+        <br>
+        <!--  
+            transition は１つの要素のみにしか適用できない
+            ２つの div を含ませると console にエラーが吐かれます
+          -->
+        <transition>
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
