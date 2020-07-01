c0f8c480444975aa3ac65ab53a16b1f22b4a01b2
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-01 23:24:10 +0900

    :sparkles: 185.Creating a Global Mixin (Special Case!)

--- vue-js-2-the-complete-guide/section13/13-1/src/main.js
+++ vue-js-2-the-complete-guide/section13/13-1/src/main.js
@@ -5,6 +5,20 @@ Vue.filter('to-lowercase', function(value) {
   return value.toLowerCase()
 })
 
+/* 
+    3回実行されます……なぜ？
+    Vueのインスタンスごとに実行されるため下の new Vue と App.vue と List.vue
+    で実行されるためです
+    サードパーティのコンポーネントを含んでいる、すべての単一の作成されたVueインスタンス
+    に影響があるため、グローバルミックスインは多用しないこと console.log を確認すると
+    全部のコンポーネントで実行されていることが確認できます
+ */
+Vue.mixin({
+  created() {
+    console.log('Global Mixin - Created Hook')
+  }
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
