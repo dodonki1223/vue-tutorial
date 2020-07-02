ddc12b84c3eb0e60cc16049f96356d4028c04a47
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-02 09:08:09 +0900

    :sparkles: 197.Mixing Transition and Animation Properties

--- vue-js-2-the-complete-guide/section14/14-1/src/App.vue
+++ vue-js-2-the-complete-guide/section14/14-1/src/App.vue
@@ -17,8 +17,11 @@
         </transition>
         <!--  
             CSSアニメーションプロパティを使用した方法
+            トランジションとアニメーションの両方を使う場合はVue.jsがどちらを使っているかわからないので
+            明示的に type 属性を使用して指定すること （※これを指定しないと奇妙な動きになります）
+            詳しくは:https://jp.vuejs.org/v2/guide/transitions.html#%E3%83%88%E3%83%A9%E3%83%B3%E3%82%B8%E3%82%B7%E3%83%A7%E3%83%B3%E3%81%A8%E3%82%A2%E3%83%8B%E3%83%A1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%82%92%E4%B8%A1%E6%96%B9%E4%BD%BF%E3%81%86
           -->
-        <transition name="slide">
+        <transition name="slide" type="animation">
           <div class="alert alert-info" v-if="show">This is some Info</div>
         </transition>
       </div>
@@ -64,10 +67,12 @@ export default {
     CSSアニメーションプロパティを使用した方法
  */
 .slide-enter {
+  opacity: 0;
 }
 
 .slide-enter-active {
   animation: slide-in 1s ease-out forwards;
+  transition: opacity .5s;
 }
 
 .slide-leave {
@@ -75,6 +80,8 @@ export default {
 
 .slide-leave-active {
   animation: slide-out 1s ease-out forwards;
+  transition: opacity 3s;
+  opacity: 0;
 }
 
 @keyframes slide-in {
