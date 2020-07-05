911915f4f5d1ca5b44c43f1499fbd855b00795a9
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-05 23:57:39 +0900

    :sparkles: Quiz-App アニメーション追加前

--- vue-js-2-the-complete-guide/section14/quiz-app/src/App.vue
+++ vue-js-2-the-complete-guide/section14/quiz-app/src/App.vue
@@ -7,7 +7,7 @@
   </div>
   <div class="row">
     <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"></div>
-      <component :is="mode"></component>
+      <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
   </div>
 </div>
 </template>
@@ -20,6 +20,16 @@ export default {
   data() {
     return {
       mode: 'app-question',
+    }
+  },
+  methods: {
+    answered(isCorrect) {
+      if (isCorrect) {
+        this.mode = 'app-answer'
+      } else {
+        this.mode = 'app-question'
+        alert('Wrong, try again!')
+      }
     }
   },
   components: {
