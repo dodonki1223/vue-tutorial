8ea645ecb66dd6a5e54899e42c2ab1244dc459c7
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-09 08:50:34 +0900

    :sparkles: 235.Setting Up and Loading Routes

--- vue-js-2-the-complete-guide/section16/16–1/src/App.vue
+++ vue-js-2-the-complete-guide/section16/16–1/src/App.vue
@@ -1,16 +1,21 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Routing</h1>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Routing</h1>
+        <!-- 
+            名前付きビュー
+              詳しくはこちら: https://router.vuejs.org/ja/guide/essentials/named-views.html#%E3%83%8D%E3%82%B9%E3%83%88%E3%81%95%E3%82%8C%E3%81%9F%E5%90%8D%E5%89%8D%E4%BB%98%E3%81%8D%E3%83%93%E3%83%A5%E3%83%BC
+          -->
+        <router-view></router-view>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-    }
+export default {
+}
 </script>
 
 <style>
--- vue-js-2-the-complete-guide/section16/16–1/src/main.js
+++ vue-js-2-the-complete-guide/section16/16–1/src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import { routes } from './routes'
+
+Vue.use(VueRouter)
+
+
+/*
+  下記書き方は以下の省略の書き方です
+    const router = new VueRouter({
+      routes: routes
+    })
+ */ 
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
