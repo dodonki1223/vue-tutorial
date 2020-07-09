8c68b7389339b15c0b8cab473e7ddefb7ccbbe82
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-09 09:42:34 +0900

    :sparkles: 238.Where am I? - Styling Active Links

--- vue-js-2-the-complete-guide/section16/16–1/src/components/Header.vue
+++ vue-js-2-the-complete-guide/section16/16–1/src/components/Header.vue
@@ -4,8 +4,12 @@
         router-link は <a> に置き換えられます 
         ページのリロードが走らないです
         Vue-Routerがなんだかんだいってやってくれている
+
+        exact を追加することで URL の完全一致になります
+          /user/id のようなものの場合は user リンクがアクティブになっているのが望ましいので包含的なマッチでアクティブになっている
+          このドキュメントを参考にするとよい: https://router.vuejs.org/ja/api/#exact
       -->
-    <li class="nav-item"><router-link to="/">Home</router-link></li>
-    <li class="nav-item"><router-link to="/user">User</router-link></li>
+    <router-link to="/"     tag="li" active-class="active" exact><a>Home</a></router-link>
+    <router-link to="/user" tag="li" active-class="active"      ><a>User</a></router-link>
   </ul>
 </template>
