c397d067f1d2504d5152c2a8b3cacf718711b4d8
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-09 09:19:41 +0900

    :sparkles: 237.Navigating with Router Links

--- vue-js-2-the-complete-guide/section16/16–1/src/App.vue
+++ vue-js-2-the-complete-guide/section16/16–1/src/App.vue
@@ -3,6 +3,8 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Routing</h1>
+        <hr>
+        <app-header></app-header>
         <!-- 
             名前付きビュー
               詳しくはこちら: https://router.vuejs.org/ja/guide/essentials/named-views.html#%E3%83%8D%E3%82%B9%E3%83%88%E3%81%95%E3%82%8C%E3%81%9F%E5%90%8D%E5%89%8D%E4%BB%98%E3%81%8D%E3%83%93%E3%83%A5%E3%83%BC
@@ -14,7 +16,12 @@
 </template>
 
 <script>
+import Header from './components/Header.vue'
+
 export default {
+  components: {
+    appHeader: Header
+  }
 }
 </script>
 
---
+++ vue-js-2-the-complete-guide/section16/16–1/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul class="nav nav-pills">
+    <!-- 
+        router-link は <a> に置き換えられます 
+        ページのリロードが走らないです
+        Vue-Routerがなんだかんだいってやってくれている
+      -->
+    <li class="nav-item"><router-link to="/">Home</router-link></li>
+    <li class="nav-item"><router-link to="/user">User</router-link></li>
+  </ul>
+</template>
