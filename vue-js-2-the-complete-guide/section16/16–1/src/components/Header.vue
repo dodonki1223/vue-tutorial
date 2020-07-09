01e8e4956019980132de27a8cd5104d04cbe895b
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-10 08:03:43 +0900

    :sparkles: 240.Setting Up Route Parameters

--- vue-js-2-the-complete-guide/section16/16–1/src/components/Header.vue
+++ vue-js-2-the-complete-guide/section16/16–1/src/components/Header.vue
@@ -9,7 +9,7 @@
           /user/id のようなものの場合は user リンクがアクティブになっているのが望ましいので包含的なマッチでアクティブになっている
           このドキュメントを参考にするとよい: https://router.vuejs.org/ja/api/#exact
       -->
-    <router-link to="/"     tag="li" active-class="active" exact><a>Home</a></router-link>
-    <router-link to="/user" tag="li" active-class="active"      ><a>User</a></router-link>
+    <router-link to="/"        tag="li" active-class="active" exact><a>Home</a></router-link>
+    <router-link to="/user/10" tag="li" active-class="active"      ><a>User</a></router-link>
   </ul>
 </template>
