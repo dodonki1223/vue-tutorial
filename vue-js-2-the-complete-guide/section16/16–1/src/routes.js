50d8ad958358473f93d141553df9a45e47e5903e
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-11 02:17:55 +0900

    :sparkles: 258.Loading Routes Lazily

--- vue-js-2-the-complete-guide/section16/16–1/src/routes.js
+++ vue-js-2-the-complete-guide/section16/16–1/src/routes.js
@@ -1,9 +1,37 @@
-import User from './components/user/User.vue'
-import UserStart from './components/user/UserStart.vue'
-import UserDetail from './components/user/UserDetail.vue'
-import UserEdit from './components/user/UserEdit.vue'
 import Home from './components/Home.vue'
 import Header from './components/Header.vue'
+
+/*
+    下記書き方を以下のように書き換える
+      import User from './components/user/User.vue'
+      import UserStart from './components/user/UserStart.vue'
+      import UserDetail from './components/user/UserDetail.vue'
+      import UserEdit from './components/user/UserEdit.vue'
+    const で宣言したものが読み込まれたタイミングでよばれる（build.js が小さいファイルで読み込まれるようになる）
+    つまり遅延評価になります（ruby の RSpec の let と同じ仕組みっぽい）
+    webpack が解釈してくれるらしい……後で調べて要調査
+      importだけだと常に大きいbuild.js が読み込まれてしまうためパフォーマンスが改善しそうだ！
+ */
+const User = resolve => {
+  require.ensure(['./components/user/User.vue'], () => {
+    resolve(require('./components/user/User.vue'))
+  }, 'user')
+}
+const UserStart = resolve => {
+  require.ensure(['./components/user/UserStart.vue'], () => {
+    resolve(require('./components/user/UserStart.vue'))
+  }, 'user')
+}
+const UserEdit = resolve => {
+  require.ensure(['./components/user/UserEdit.vue'], () => {
+    resolve(require('./components/user/UserEdit.vue'))
+  }, 'user')
+}
+const UserDetail = resolve => {
+  require.ensure(['./components/user/UserDetail.vue'], () => {
+    resolve(require('./components/user/UserDetail.vue'))
+  }, 'user')
+}
 
 export const routes = [
   { path: '', name: 'home', components: {
