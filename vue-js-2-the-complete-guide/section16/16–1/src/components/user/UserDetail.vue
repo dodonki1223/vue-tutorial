55127de3356baefb1be360402319d924ec1a0b31
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-10 10:00:54 +0900

    :sparkles: 247.A Better Way of Creating Links - With Named Routes

--- vue-js-2-the-complete-guide/section16/16–1/src/components/user/UserDetail.vue
+++ vue-js-2-the-complete-guide/section16/16–1/src/components/user/UserDetail.vue
@@ -2,9 +2,15 @@
   <div>
     <h3>Some User Details</h3>
     <p>User loaded has ID: {{ $route.params.id }}</p>
+    <!--  
+        遷移先の設定として以下の２つのやり方がある
+          - "`/user/${$route.params.id}/edit`"
+          - "{ name: 'userEdit', params: { id: $route.params.id } }"
+        文字列でパスを生成する方法と routes.js で設定した名前付きルートを使う方法
+      -->
     <router-link 
       tag="button" 
-      :to="`/user/${$route.params.id}/edit`"
+      :to="{ name: 'userEdit', params: { id: $route.params.id } }"
       class="btn btn-primary">Edit User</router-link>
   </div>
 </template>
