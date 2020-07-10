2c8746e744291deb610aa07b66a641f243582764
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-10 23:53:53 +0900

    :sparkles: 249.Multiple Router Views (Named Router Views)

--- vue-js-2-the-complete-guide/section16/16–1/src/App.vue
+++ vue-js-2-the-complete-guide/section16/16–1/src/App.vue
@@ -4,12 +4,15 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Routing</h1>
         <hr>
-        <app-header></app-header>
         <!-- 
             名前付きビュー
               詳しくはこちら: https://router.vuejs.org/ja/guide/essentials/named-views.html#%E3%83%8D%E3%82%B9%E3%83%88%E3%81%95%E3%82%8C%E3%81%9F%E5%90%8D%E5%89%8D%E4%BB%98%E3%81%8D%E3%83%93%E3%83%A5%E3%83%BC
+            名前付きルータービュー
+              コード内が見やすくなる
           -->
+        <router-view name="header-top"></router-view>
         <router-view></router-view>
+        <router-view name="header-bottom"></router-view>
       </div>
     </div>
   </div>
