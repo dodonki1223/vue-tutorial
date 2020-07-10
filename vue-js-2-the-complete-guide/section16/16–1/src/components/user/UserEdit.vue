bbbe3283fcfd9c4b5f6064e27f83ff738fec8886
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-11 01:59:02 +0900

    :sparkles: 257.Using the "beforeLeave" Guard

--- vue-js-2-the-complete-guide/section16/16–1/src/components/user/UserEdit.vue
+++ vue-js-2-the-complete-guide/section16/16–1/src/components/user/UserEdit.vue
@@ -4,8 +4,33 @@
     <p>Locale: {{ $route.query.locale }}</p>
     <p>Analytics: {{ $route.query.q }}</p>
     <hr>
-    <button class="btn btn-primary">Confirm</button>
+    <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
     <div style="height: 700px"></div>
     <p id="data">Some extra Data</p>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      confirmed: false
+    }
+  },
+  beforeRouteLeave(to, from, next) {
+    /* 
+        編集が完了しているかどうかで画面の遷移ができるかどうかを制御している
+          confirm ボタンを押した時はそのまま遷移する
+          confirm ボタンを押していない時は confirm を表示させて遷移を制御する
+     */
+    if (this.confirmed) {
+      next()
+    } else {
+      if (confirm('Are you sure?')) {
+        next()
+      } else {
+        next(false)
+      }
+    }
+  }
+}
+</script>
