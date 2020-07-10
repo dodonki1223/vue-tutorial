09d51a7e54a3339a35b8060861c1c4d43eff984d
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-10 09:26:21 +0900

    :sparkles: 244.Setting Up Child Routes (Nested Routes)

--- vue-js-2-the-complete-guide/section16/16–1/src/components/user/User.vue
+++ vue-js-2-the-complete-guide/section16/16–1/src/components/user/User.vue
@@ -3,7 +3,8 @@
     <h1>The User Page</h1>
     <hr>
     <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
-    <p>Loaded ID: {{ id }}</p>
+    <hr>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -22,16 +23,6 @@
       https://github.com/vuejs/vue-router/tree/dev/examples/route-props
  */
 export default {
-  data() {
-    return {
-      id: this.$route.params.id
-    }
-  },
-  watch: {
-    '$route'(to, from) {
-      this.id = to.params.id
-    }
-  },
   methods: {
     navigateToHome() {
       this.$router.push('/')
