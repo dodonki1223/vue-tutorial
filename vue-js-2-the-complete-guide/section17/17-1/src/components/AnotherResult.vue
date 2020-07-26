df60b59ad27325d3b2821dd0f6b9b5957ae43c4c
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-27 08:50:09 +0900

    :sparkles: 268. Mapping Getters to Properties
    - Vuex の Getters プロパティに簡単にアクセスできるようにするために mapGetters を使用する
    - スプレッド構文を使用して 簡単にアクセスできるようにする

--- vue-js-2-the-complete-guide/section17/17-1/src/components/AnotherResult.vue
+++ vue-js-2-the-complete-guide/section17/17-1/src/components/AnotherResult.vue
@@ -1,13 +1,43 @@
 <template>
-    <p>Counter is: {{ counter }}</p>
+    <div>
+      <!-- 
+        <別解>
+        <p>Counter is: {{ counter }}</p>
+        <p>Number of Clicks: {{ clicks }}</p>
+      -->
+      <p>Counter is: {{ doubleCounter }}</p>
+      <p>Number of Clicks: {{ stringCounter }}</p>
+    </div>
 </template>
 
 <script>
+    import { mapGetters } from 'vuex';
     export default {
-        computed: {
-            counter() {
-                return this.$store.getters.doubleCounter;
+        /*
+          mapGetters を使用した方がスッキリすると思う！
+          <別解>
+            computed: {
+                counter() {
+                    return this.$store.getters.doubleCounter;
+                },
+                clicks() {
+                    return this.$store.getters.stringCounter;
+                }
             }
+
+        これでもいいが、新しい computed を追加することができないので
+        スプレッド構文を使用することで他の computed を追加することができるようになる
+          computed: mapGetters([
+            'doubleCounter',
+            'stringCounter'
+          ])
+         */
+        computed: {
+          ...mapGetters([
+            'doubleCounter',
+            'stringCounter'
+          ])
         }
+
     }
 </script>
