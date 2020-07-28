3db0fc7aa1569356d6e576c88cf09d22241c5f3e
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-07-28 22:52:56 +0900

    :sparkles: 279. Using Separate Files
    - getters, mutations, actions を別ファイルとして切り出す

--- vue-js-2-the-complete-guide/section17/17-1/src/store/store.js
+++ vue-js-2-the-complete-guide/section17/17-1/src/store/store.js
@@ -2,27 +2,28 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import counter from './modules/counter';
 
+/* 
+    「import * as name」構文はすべてをインポートする
+    下記のコードは、myModule を現在のスコープに加え、/modules/my-module.js のファイルの
+    モジュールからのエクスポートすべてを含めます。
+      import * as myModule from '/modules/my-module.js';
+    エクスポートにアクセスするには、モジュール名（ここでは「myModule」）を名前空間として用いることになります。 
+    たとえば、上記でインポートされたモジュールがエクスポートにdoAllTheAmazingThings()を含む場合は、下記のように呼び出します。
+      myModule.doAllTheAmazingThings();
+ */
+import * as actions from './actions';
+import * as getters from './getters';
+import * as mutations from './mutaions';
+
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
     value: 0
   },
-  getters: {
-    value: state => {
-      return state.value;
-    }
-  },
-  mutations: {
-    updateValue: (state, payload) => {
-      state.value = payload;
-    }
-  },
-  actions: {
-    updateValue({ commit }, payload) {
-      commit('updateValue', payload);
-    }
-  },
+  getters,
+  mutations,
+  actions,
   modules: {
     counter
   }
