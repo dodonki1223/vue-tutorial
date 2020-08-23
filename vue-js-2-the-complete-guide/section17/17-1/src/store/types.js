33ff3f69f6775a9045f5bab87cd633ebc59d9df6
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-08-23 22:11:31 +0900

    :memo: 説明文を追加

--- vue-js-2-the-complete-guide/section17/17-1/src/store/types.js
+++ vue-js-2-the-complete-guide/section17/17-1/src/store/types.js
@@ -1,2 +1,7 @@
+/*
+  Vuex2.1 を使用している場合は名前空間を手動で設定する必要がありません
+  下記のやり方さえ知っていればこの types.js のやり方を使わなくてもOKです！
+  https://github.com/vuejs/vuex/releases/tag/v2.1.0
+ */
 export const DOUBLE_COUNTER ='counter/DOUBLE_COUNTER';
 export const CLICK_COUNTER ='counter/CLICK_COUNTER';
